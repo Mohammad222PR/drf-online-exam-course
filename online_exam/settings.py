e589a6aab308261acc3e55b080c1b68e4c01f1d7
Mohammad222PR <mohammades13851@gmail.com>
2024-07-24 23:50:57 +0330

    feat: install and setup python decouple package

--- online_exam/settings.py
+++ online_exam/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-4_-l%f@9%v%+ts(do6gcacwnd0re96wo70v6cte_58)1qd^0hx"
+SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
