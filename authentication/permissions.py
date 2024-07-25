6599ac98b18f25ecbc4ab335e439d85e4faa5705
Mohammad222PR <mohammades13851@gmail.com>
2024-07-25 19:45:51 +0330

    feat: create the IsInstructorOwner permission class

--- authentication/permissions.py
+++ authentication/permissions.py
@@ -4,6 +4,13 @@ from rest_framework.permissions import SAFE_METHODS, BasePermission
 class IsInstructor(BasePermission):
     def has_permission(self, request, view):
         return request.user.is_instructor
+
+
+class IsInstructorOwner(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return request.user.id == obj.instructor_id
+
+
 class IsStudent(BasePermission):
     def has_permission(self, request, view):
         return request.user.is_student
