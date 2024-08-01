d5469a457c64ee26ab297c0486f135427f201d42
Mohammad222PR <mohammades13851@gmail.com>
2024-08-01 15:11:37 +0330

    feat: create the IsAdminOrInstructor permission class

--- authentication/permissions.py
+++ authentication/permissions.py
@@ -9,6 +9,12 @@ class IsStudent(BasePermission):
     def has_permission(self, request, view):
         return request.user.is_student
 
+
+class IsAdminOrInstructor(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_staff or request.user.is_instructor
+
+
 class IsInstructorOwner(BasePermission):
     def has_object_permission(self, request, view, obj):
         return request.user.id == obj.instructor_id
