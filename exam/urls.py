b9c90e44826ece40b9202e1aaf10274dbdd233fd
Mohammad222PR <mohammades13851@gmail.com>
2024-08-01 18:02:39 +0330

    feat: register exam, question, scores and answers urls

--- exam/urls.py
+++ exam/urls.py
@@ -1,5 +1,13 @@
 from rest_framework.routers import DefaultRouter
-from .views import category_views,exam_views
+from .views import (
+    answer_views,
+    category_views,
+    exam_views,
+    questions_views,
+    score_views,
+)
+from rest_framework_nested import routers
+
 urlpatterns = []
 
 router = DefaultRouter()
@@ -15,5 +23,25 @@ router.register(
 
 router.register(r"exams", exam_views.ExamViewSet, basename="exams")
 
+router.register(r"questions", questions_views.QuestionViewSet, basename="questions")
+
+router.register(r"scores", score_views.ScoreViewSet, basename="scores")
+
+router.register(r"my-scores", score_views.MyScoreViewSet, basename="my-scores")
+
+
+############# Nested routers #############
+exam_router = routers.NestedSimpleRouter(router, r"exams", lookup="exam")
+
+exam_router.register(
+    r"questions", questions_views.ExamQuestionViewSet, basename="questions"
+)
+
+exam_router.register(r"answers", answer_views.AnswerViewSet, basename="answers")
+
+exam_router.register(r"scores", score_views.ExamScoreViewSet, basename="exam-scores")
+
+
 urlpatterns += router.urls
 
+urlpatterns += exam_router.urls
