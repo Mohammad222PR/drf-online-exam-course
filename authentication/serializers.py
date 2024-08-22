1449dc6668d0f373bb501fe56ff15793abc528f6
Mohammad222PR <mohammades13851@gmail.com>
2024-08-23 01:43:27 +0330

    refactor: add a field to specify roles

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,6 +1,15 @@
 from djoser.serializers import UserCreateSerializer as BaseUserCreateSerializer
+from rest_framework import serializers
+from account.choices import USER_ROLE_STUDENT, USER_ROLE_INSTRUCTOR
 
 
 class UserCreateSerializer(BaseUserCreateSerializer):
+    role = serializers.ChoiceField(
+        choices=(
+            (USER_ROLE_INSTRUCTOR, "مربی"),
+            (USER_ROLE_STUDENT, "دانش اموز"),
+        )
+    )
+
     class Meta(BaseUserCreateSerializer.Meta):
         fields = BaseUserCreateSerializer.Meta.fields + ("role",)
